35b400e2746d1c2d1264fc27f36386f6b1ba7588
Randall Gaz <rgaz@rgaz.info>
2024-10-07 17:35:39 -0600

    sort chars

--- main.py
+++ main.py
@@ -2,15 +2,20 @@
 def main():
     with open('books/frankenstein.txt') as f:
         content = f.read().lower()
-    print(content)
+#    print(content)
     chars_dict = dict()
     for c in content:
-        if (not c in chars_dict):
-            chars_dict[c] = 0
-        chars_dict[c] += 1
+        if (c.isalpha()):
+            if (not c in chars_dict):
+                chars_dict[c] = 0
+            chars_dict[c] += 1
     ordered_dict = dict(sorted(chars_dict.items(), reverse=True, key=lambda item: item[1]))
-    print(ordered_dict)
+ #   print( ordered_dict.items())
+#    print(ordered_dict)
 
+    for entry in ordered_dict.items():
+        print(entry)
+        
 main()
 
 
